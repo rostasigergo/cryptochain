63b394a015d86fd4bcb96b8429f09c90ca44222b
RoGer <rostasigergo@gmail.com>
2019-04-02 21:25:56 +0200

    create & update transaction functions

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -84,11 +84,42 @@ describe('Transaction', () => {
             describe('and the transaction input signature is invalid', () => {
                 it('returns false and logs an error', () => {
                     transaction.input.signature = new Wallet().sign('data');
-                    
+
                     expect(Transaction.validTransaction(transaction)).toBe(false);
                     expect(errorMock).toHaveBeenCalled();
                 });
             });
+        });
+    });
+
+    describe('update()', () => {
+        let originalSignature, originalSenderOutput, nextRecipient, nextAmount;
+
+        beforeEach(() => {
+            originalSignature = transaction.input.signature;
+            originalSenderOutput = transaction.outputMap[senderWallet.publicKey];
+            nextRecipient = 'next-recipient';
+            nextAmount = 50;
+
+            transaction.update({senderWallet, recipient: nextRecipient, amount: nextAmount});
+        });
+
+        it('ouputs the amount to the next recipient', () => {
+            expect(transaction.outputMap[nextRecipient]).toEqual(nextAmount);
+        });
+
+        it('subtracts the amount from the original sender output amount', () => {
+            expect(transaction.outputMap[senderWallet.publicKey]).toEqual(originalSenderOutput - nextAmount);
+        });
+
+        it('maintains a total output that matches the input amount', () => {
+            expect(
+                Object.values(transaction.outputMap).reduce((total, outputAmount) => total + outputAmount)
+                ).toEqual(transaction.input.amount);
+        });
+
+        it('re-signs the transaction', () => {
+            expect(transaction.input.signature).not.toEqual(originalSignature);
         });
     });
 
