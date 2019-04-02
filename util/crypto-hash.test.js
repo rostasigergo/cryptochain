e204d2f589092e6e1d341882a219d3daa3c419bd
RoGer <rostasigergo@gmail.com>
2019-04-02 21:25:18 +0200

    Upgrade cryptoHash function

--- util/crypto-hash.test.js
+++ util/crypto-hash.test.js
@@ -3,14 +3,22 @@ const cryptoHash = require('./crypto-hash');
 
 describe('cryptoHash()', () => {
     it('generates a SHA-256 hashed output for ´foo´', () => {
-        expect(cryptoHash('foo')).toEqual('2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae')
+        expect(cryptoHash('foo')).toEqual('b2213295d564916f89a6a42455567c87c3f480fcd7a1c15e220f17d7169a790b')
     });
 
     it('generates a SHA-256 hashed output for ˙bar˙', () => {
-        expect(cryptoHash('bar')).toEqual('fcde2b2edba56bf408601fb721fe9b5c338d10ee429ea04fae5511b68fbf8fb9')
+        expect(cryptoHash('bar')).toEqual('4c293ff010a730f0972761331d1b5678478d425c2dc5cefd16d8f20059e497f3')
     });
 
     it('produces the same hash with the same input arguments in any order', () => {
         expect(cryptoHash('one', 'two', 'three')).toEqual(cryptoHash('three', 'one', 'two'));
     })
+
+    it('produces a unique hash when the properties have changed on an input', () => {
+        const foo = {};
+        const originalHash = cryptoHash(foo);
+        foo['a'] = 'a';
+
+        expect(cryptoHash(foo)).not.toEqual(originalHash);
+    });
 });
