8d9a380040a1d247e8cf89c5f1af972763fba42d
Roger <rostasigergo@gmail.com>
2019-04-09 22:40:12 +0200

    Transaction pool part 1

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
+
 const PubSub = require('./app/pubsub');
 
 const app = express();
 const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain });
 
 const DEFAULT_PORT = 3000;
@@ -26,6 +31,18 @@ app.post('/api/mine', (req, res) => {
     pubsub.broadcastChain();
 
     res.redirect('/api/blocks');
+});
+
+app.post('/api/transact', (req, res) => {
+    const { amount, recipient } = req.body;
+
+    const transaction = wallet.createTransaction({ recipient, amount });
+
+    transactionPool.setTransaction(transaction);
+
+    console.log('transactionPool', transactionPool);
+
+    res.json({transaction});
 });
 
 
