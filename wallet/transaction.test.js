fa0c10fc296c52ee1afd8dea750fd88224edc927
RoGer <rostasigergo@gmail.com>
2019-04-14 22:01:23 +0200

    Mine transactions part 1

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -1,6 +1,7 @@
 const Transaction = require('./transaction');
 const Wallet = require('./index');
 const { verifySignature } = require('../util');
+const { REWARD_INPUT, MINING_REWARD } = require('../config');
 
 describe('Transaction', () => {
     let transaction, senderWallet, recipient, amount;
@@ -150,6 +151,23 @@ describe('Transaction', () => {
             });
         });
 
+    });
+
+    describe('rewardTransaction()', () => {
+        let rewardTransaction, minerWallet;
+
+        beforeEach(() => {
+            minerWallet = new Wallet();
+            rewardTransaction = Transaction.rewardTransaction({ minerWallet });
+        });
+
+        it('creates a transaction with the reward input', () => {
+            expect(rewardTransaction.input).toEqual(REWARD_INPUT);
+        });
+
+        it('creastes ones transaction for the miner with the `MINING REWARD`', () => {
+            expect(rewardTransaction.outputMap[minerWallet.publicKey]).toEqual(MINING_REWARD);
+        });
     });
 
 });
