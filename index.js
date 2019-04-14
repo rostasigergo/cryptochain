a8fdec1d426c31a8af45a378b0b543dfc84abee5
RoGer <rostasigergo@gmail.com>
2019-04-14 22:23:59 +0200

    MineTransaction part 2

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
+const TransactionMiner = require('./app/transaction-miner');
 
 const PubSub = require('./app/pubsub');
 
@@ -12,6 +13,7 @@ const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain, transactionPool, wallet });
+const transactionMiner = new TransactionMiner({blockchain, transactionPool, wallet, pubsub});
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -60,6 +62,12 @@ app.post('/api/transact', (req, res) => {
 
 app.get('/api/transaction-pool-map', (req, res) => {
     res.json(transactionPool.transactionMap);
+});
+
+app.get('/api/mine-transactions', (req, res) => {
+    transactionMiner.mineTransaction();
+
+    res.redirect('/api/blocks');
 });
 
 
