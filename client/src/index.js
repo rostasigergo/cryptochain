49e66138fd8b1b7a6d19edd430514928bc22e7e6
RoGer <rostasigergo@gmail.com>
2019-04-26 07:22:48 +0200

    Frontend part 1

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/App';
+import './index.css';
 
 render(
-    <div>Cryptochain in React!</div>, 
+    <App />, 
     document.getElementById('root')
 );
